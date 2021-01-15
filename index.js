78167e659dd58487e66537178695dabe9c6d5c89
KaroseLiu <kusoroselh@gmail.com>
2021-01-15 16:12:57 +0800

    Add the sendBatch

--- index.js
+++ index.js
@@ -1,78 +1,77 @@
-const utils = require('./utils/encrypt');
-var rp = require('request-promise');
+const utils = require('./utils/encrypt')
+var rp = require('request-promise')
 
 module.exports = {
-    send: async function (endpoint, data) {
-        let postData = {
-            client_id: data.client_id,
-            rec: data.rec,
-            prefix: data.prefix,
-            template: data.template,
-            params: data.params
-        };
-        let sign = utils.encrypt(postData, data.client_key);
-
-        postData.sign = sign;
-
-
-        let options = {
-            method: 'POST',
-            uri: `${endpoint}/sms/send`,
-            body: postData,
-            json: true
-        };
-        let rst = await rp(options);
-        return rst;
-    },
-    code: async function (endpoint, data) {
-        let postData = data;
-        let sign = utils.encrypt(postData, data.client_key);
-
-        postData.sign = sign;
-        delete postData.client_key;
-        let options = {
-            method: 'POST',
-            uri: `${endpoint}/sms/code`,
-            body: postData,
-            json: true
-        }
-        let rst = await rp(options);
-        return rst;
-    },
-    sendBatch: async function(endpoint, data) {
-        let postData = {
-          client_id: data.client_id,
-          PhoneNumberJson: data.PhoneNumberJson,
-          SignNameJson: data.SignNameJson,
-          template: data.template,
-          TemplateParamJson: data.TemplateParamJson,
-        }
-        let sign = utils.encrypt(postData, data.client_key)
-    
-        postData.sign = sign
-    
-        let options = {
-          method: 'POST',
-          uri: `${endpoint}/sms/sendBatch`,
-          body: postData,
-          json: true,
-        }
-        let rst = await rp(options)
-        return rst
-      },
-    verify: async function (endpoint, data) {
-        let postData = data;
-        let sign = utils.encrypt(postData, data.client_key);
-
-        postData.sign = sign;
-        delete postData.client_key;
-        let options = {
-            method: 'POST',
-            uri: `${endpoint}/sms/verify`,
-            body: postData,
-            json: true
-        }
-        let rst = await rp(options);
-        return rst;
+  send: async function (endpoint, data) {
+    let postData = {
+      client_id: data.client_id,
+      rec: data.rec,
+      prefix: data.prefix,
+      template: data.template,
+      params: data.params,
     }
+    let sign = utils.encrypt(postData, data.client_key)
+
+    postData.sign = sign
+
+    let options = {
+      method: 'POST',
+      uri: `${endpoint}/sms/send`,
+      body: postData,
+      json: true,
+    }
+    let rst = await rp(options)
+    return rst
+  },
+  code: async function (endpoint, data) {
+    let postData = data
+    let sign = utils.encrypt(postData, data.client_key)
+
+    postData.sign = sign
+    delete postData.client_key
+    let options = {
+      method: 'POST',
+      uri: `${endpoint}/sms/code`,
+      body: postData,
+      json: true,
+    }
+    let rst = await rp(options)
+    return rst
+  },
+  sendBatch: async function (endpoint, data) {
+    let postData = {
+      client_id: data.client_id,
+      rec: data.rec,
+      prefix: data.prefix,
+      template: data.template,
+      params: data.params,
+    }
+    let sign = utils.encrypt(postData, data.client_key)
+
+    postData.sign = sign
+
+    let options = {
+      method: 'POST',
+      uri: `${endpoint}/smsV2/send`,
+      body: postData,
+      json: true,
+    }
+    let rst = await rp(options)
+    return rst
+  },
+  verify: async function (endpoint, data) {
+    let postData = data
+    let sign = utils.encrypt(postData, data.client_key)
+
+    postData.sign = sign
+    delete postData.client_key
+    let options = {
+      method: 'POST',
+      uri: `${endpoint}/sms/verify`,
+      body: postData,
+      json: true,
+    }
+    let rst = await rp(options)
+    return rst
+  },
 }
